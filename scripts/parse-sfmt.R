a361f21f0ec8acad6ed0997156df7ce33dddedf6
Yoshihiro Ishida <yishida@kuhp.kyoto-u.ac.jp>
2018-09-04 15:39:54 +0900

    women included in the analysis
    this inclusion caused slight change in 2015 analysis

--- scripts/parse-sfmt.R
+++ scripts/parse-sfmt.R
@@ -1,14 +1,22 @@
 library(tidyverse)
 library(tabulizer)
 
-sfmt2015_raw <- extract_tables("input/result2015m.pdf", encoding = 'UTF-8')
+sfmt2015_rawm <- extract_tables("input/result2015m.pdf", encoding = 'UTF-8')
+sfmt2015_rawf <- extract_tables("input/result2015w.pdf", encoding = 'UTF-8')
+
+sfmt2015_raw <- c(sfmt2015_rawm, sfmt2015_rawf)
+
 sfmt2015_tb <- map(sfmt2015_raw, as.tibble)
 sfmt2015 <- bind_rows(sfmt2015_tb) %>% 
   set_names(c("rank2015", "bib", "name_kanji", "name_kana", "madarao", "kurohime", "otomi", "ohashi", "togakushi", "finish")) %>% 
   mutate_all(parse_guess) %>% 
   mutate_at(vars(madarao:finish), as.numeric)
 
-sfmt2017_raw <- extract_tables("input/result2017_m160.pdf", encoding = 'UTF-8')
+sfmt2017_rawm <- extract_tables("input/result2017_m160.pdf", encoding = 'UTF-8')
+sfmt2017_rawf <- extract_tables("input/result2017_w160.pdf", encoding = 'UTF-8')
+
+sfmt2017_raw <- c(sfmt2017_rawm, sfmt2017_rawf)
+
 sfmt2017_tb <- map(sfmt2017_raw, as.tibble)
 sfmt2017 <- bind_rows(sfmt2017_tb) %>% 
   set_names(c("rank2017", "bib", "name_kanji", "name_kana", "banff", "akaike", "apa", "seishonen","akakura", "kurohime")) %>% 
